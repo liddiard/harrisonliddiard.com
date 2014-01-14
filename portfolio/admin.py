d9ece4036263d97e330bec63857cf9910b2eb93c
omniaura <omniaura5@gmail.com>
2014-01-14 00:21:39 -0800

    registered models in admin

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Project, Image, Skill
 
-# Register your models here.
+
+class ProjectAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Project, ProjectAdmin)
+
+
+class ImageAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Image, ImageAdmin)
+
+
+class SkillAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Skill, SkillAdmin)
