7c07f74da7ac0a198f960f385a56c0e93f351595
omniaura <omniaura5@gmail.com>
2014-01-15 09:18:17 -0800

    added boto and django-storages to requirements, s3 settings

--- harrisonliddiard/settings/base.py
+++ harrisonliddiard/settings/base.py
@@ -94,3 +94,11 @@ STATIC_URL = '/static/'
 TEMPLATE_DIRS = (
     BASE_DIR+'/templates',
 )
+
+AWS_STORAGE_BUCKET_NAME = "harrisonliddiard"
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+AWS_PRELOAD_METADATA = True
+STATICFILES_STORAGE = "storages.backends.s3boto.S3BotoStorage"
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto.S3BotoStorage"
+S3_URL = "http://%s.s3.amazonaws.com/" % AWS_STORAGE_BUCKET_NAME
