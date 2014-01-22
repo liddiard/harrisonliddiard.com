9d95688bbf7e228f42b028cf611936b012cf8101
omniaura <omniaura5@gmail.com>
2014-01-22 10:59:33 -0800

    trying to fix MEDIA_URL/mediafiles location

--- harrisonliddiard/settings/prod.py
+++ harrisonliddiard/settings/prod.py
@@ -12,11 +12,10 @@ AWS_STORAGE_BUCKET_NAME = "harrisonliddiard"
 AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 AWS_PRELOAD_METADATA = True
-STATICFILES_STORAGE = "storages.backends.s3boto.S3BotoStorage"
-DEFAULT_FILE_STORAGE = "storages.backends.s3boto.S3BotoStorage"
+DEFAULT_FILE_STORAGE = 'portfolio.s3utils.MediaRootS3BotoStorage'
+STATICFILES_STORAGE = 'portfolio.s3utils.StaticRootS3BotoStorage'
 S3_URL = "http://%s.s3.amazonaws.com/" % AWS_STORAGE_BUCKET_NAME
 
-MEDIA_ROOT = S3_URL + 'media/'
 
-STATIC_URL = S3_URL
+STATIC_URL = S3_URL + 'static/'
 MEDIA_URL = S3_URL + 'media/'
