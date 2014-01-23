39d070d7196049912040849cf5e77fc4921d714d
omniaura <omniaura5@gmail.com>
2014-01-22 17:35:02 -0800

    got rid of blurjs, show div with opacity on hover

--- portfolio/static/js/main.js
+++ portfolio/static/js/main.js
@@ -1,11 +1,7 @@
 $(window).load(function() {
     $('section.projects').collagePlus({
-        'targetHeight': 250,
+        'targetHeight': 240,
         'allowPartialLastRow': true,   
-    });
-    $('.projects img').blurjs({
-        source: 'body',
-        radius: 7,
     });
     $('.flexslider').flexslider({
         animation: "slide"
